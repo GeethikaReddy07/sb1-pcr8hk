fd9a94165b5d22a00b3b6b407e34f8385f996b61
GeethikaReddy07 <geethikar54@gmail.com>
2024-11-06 22:08:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-pcr8hk
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/GeethikaReddy07/sb1-pcr8hk)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Colleges from './components/Colleges';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Colleges />
+    </div>
+  );
+}
+
+export default App;
